5a9ba8c9ba97d25e05856b9f033c2dfee4a36cc2
Matthew Pantel <79867801+TypeErrorDev@users.noreply.github.com>
2022-04-03 15:52:51 +0300

    Revert "resolved"
    This reverts commit c8ad2a53a42e3a70badcf30695bc3cc473911847.

--- js/app.js
+++ js/app.js
@@ -21,17 +21,40 @@ let phrases = [
   "on top of the world",
 ];
 
+// resetting the phrases
+function resetPhrase() {
+  phrase.removeChild(phrase.firstElementChild);
+  let ul = document.createElement("ul");
+  phrase.appendChild(ul);
+}
+
+// resetting scoreboard
+function resetScoreboard() {
+  for (let i = 0; i < 5; i++) {
+    consold.log("Removing Scoreboard");
+    scoreboard.removeChild(scoreboard.firstElementChild);
+    let lostHeartLi = document.createElement("li");
+    lostHeartLi.className = "tries";
+    let lostHeartImg = document.createElement("img");
+    lostHeartImg.src = "res/lostHeart.png";
+    scoreboard.appendChild(lostHeartLi);
+    lostHeartLi.appendChild(lostHeartImg);
+  }
+}
+
+// resetting qwerty
+function resetQWERTY() {
+  let qwerty = document.querySelectorAll("#qwerty button");
+  for (let i = 0; i < qwerty.length; i++) {
+    qwerty[i].disabled = false;
+    qwerty[i].classList.remove("chosen");
+  }
+}
+
 // Hides display when start button is clicked
-startButton.addEventListener("click", start);
-
-function start(e) {
+startButton.addEventListener("click", (e) => {
   overlay.style.display = "none";
-}
-
-function reset(e) {
-  restartGame(phrases);
-  console.log("Reset");
-}
+});
 
 // This function will randomly select a phrase from phraseArray
 const getRandomPhraseAsArray = (phrases) => {
@@ -41,18 +64,17 @@ const getRandomPhraseAsArray = (phrases) => {
 
 // for each character of phrase, it creates li element and appends
 // Then checks that each character is a through z, if true: add class 'letter', if false: add class 'space'
-const addPhraseToDisplay = (phrase, ul) => {
+const addPhraseToDisplay = (phrase) => {
   for (const char of phrase) {
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(char));
-    ul === undefined ? list.appendChild(li) : ul.appendChild(li);
+    list.appendChild(li);
     if (char >= "a" && char <= "z") {
       li.classList.add("letter");
     } else {
       li.classList.add("space");
     }
   }
-  console.log(list);
 };
 
 const phraseArray = getRandomPhraseAsArray(phrases);
@@ -82,6 +104,7 @@ qwerty.addEventListener("click", (event) => {
       missed++;
       scoreboard.removeChild(scoreboard.firstElementChild);
       let lostHeartLi = document.createElement("li");
+      lostHeartLi.className = "tries";
       let lostHeartImg = document.createElement("img");
       lostHeartImg.src = "res/lostHeart.png ";
       lostHeartImg.className = "imgTries";
@@ -95,63 +118,29 @@ qwerty.addEventListener("click", (event) => {
 // this checks to see if all of the letters were guess, or if you accumulated 5 misses
 // triggers win scree/loss screen
 function checkWin() {
-  let letter = document.querySelectorAll(".letter");
-  let show = document.querySelectorAll(".show");
+  let letter = document.getElementsByClassName(".letter");
+  let show = document.getElementsByClassName(".show");
   if (letter.length == show.length) {
     overlay.className = "win";
-    title.textContent = "You win!";
-    overlay.style.display = "flex";
-    console.log(letter);
-    console.log(show);
+    title.textContent = "You win my guy!";
+    overlay.display = "flex";
   }
   if (missed === 5) {
     overlay.className = "lose";
     title.textContent = "You lost bro!";
     startButton.textContent = "Try your luck again";
     overlay.style.display = "flex";
-    startButton.removeEventListener("click", start);
-    startButton.addEventListener("click", reset);
-  }
-}
-
-// resetting the phrases
-function resetPhrase(arr) {
-  phrase.removeChild(phrase.firstElementChild);
-  let ul = document.createElement("ul");
-  ul.classList.add("reset_list");
-  phrase.appendChild(ul);
-  addPhraseToDisplay(arr, ul);
-}
-
-// resetting scoreboard
-function resetScoreboard() {
-  for (let i = 0; i < 5; i++) {
-    scoreboard.removeChild(scoreboard.firstElementChild);
-    let liveHeartLi = document.createElement("li");
-    let liveHeartImg = document.createElement("img");
-    liveHeartImg.src = "res/liveHeart.png";
-    liveHeartImg.className = "imgTries";
-    scoreboard.appendChild(liveHeartLi);
-    liveHeartLi.appendChild(liveHeartImg);
-  }
-}
-
-// resetting qwerty
-function resetQWERTY() {
-  let qwerty = document.querySelectorAll("#qwerty button");
-  for (let i = 0; i < qwerty.length; i++) {
-    qwerty[i].disabled = false;
-    qwerty[i].classList.remove("chosen");
   }
 }
 
 // clearing the game
 // This clears the phrase, scoreboard and the qwerty keyboard
-function restartGame(arr) {
-  let phrases = getRandomPhraseAsArray(arr);
-  resetPhrase(phrases);
+function restartGame() {
+  resetPhrase();
   resetScoreboard();
   resetQWERTY();
   missed = 0;
-  overlay.style.display = "none";
+  let phrases = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phrases);
 }
+restartGame();
